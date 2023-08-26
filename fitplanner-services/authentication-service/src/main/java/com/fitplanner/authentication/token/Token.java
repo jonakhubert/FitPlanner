01e8e194fc566aa5810b5086d5a7ff2e0c01a871
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-26 20:40:53 +0200

    Change record to class

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/token/Token.java
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/token/Token.java
@@ -6,11 +6,32 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "tokens")
-public record Token(
-    @Id String id,
-    String token,
-    @Enumerated(EnumType.STRING) TokenType tokenType,
-    boolean expired,
-    boolean revoked,
-    String userEmail
-) {}
+public class Token {
+
+    @Id
+    private String id;
+
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private final TokenType tokenType = TokenType.BEARER;
+
+    private boolean expired;
+
+    private boolean revoked;
+
+    private String userEmail;
+
+    public Token(String token, boolean expired, boolean revoked, String userEmail) {
+        this.token = token;
+        this.expired = expired;
+        this.revoked = revoked;
+        this.userEmail = userEmail;
+    }
+
+    public void setExpired(boolean expired) { this.expired = expired; }
+    public void setRevoked(boolean revoked) { this.revoked = revoked; }
+    //public void setTokenType(TokenType tokenType) { this.tokenType = tokenType; }
+    public boolean isExpired() { return expired; }
+    public boolean isRevoked() { return revoked; }
+}
