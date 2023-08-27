f23edd932270daa0837f5fbf3c9840ad8757d030
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-27 18:55:19 +0200

    Remove unused exceptions

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/exception/model/InvalidAuthenticationTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.fitplanner.authentication.model.exception;
-
-public class InvalidAuthenticationTokenException extends RuntimeException {
-    public InvalidAuthenticationTokenException(String message) {
-        super(message);
-    }
-}
