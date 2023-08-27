fac25a50dce3f095f9e74a196dd97da1e993b42b
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-27 14:15:51 +0200

    Change mongo template to mongo repository

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/token/TokenRepository.java
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/token/TokenRepository.java
@@ -1,48 +1,13 @@
 package com.fitplanner.authentication.token;
 
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+import com.fitplanner.authentication.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class TokenRepository implements ITokenRepository {
-
-    private final MongoTemplate mongoTemplate;
-
-    public TokenRepository(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
-    }
-
-    @Override
-    public List<Token> findAllValidTokensByUser(String email) {
-        Query query = new Query(Criteria.where("userEmail").is(email)
-            .andOperator(
-                Criteria.where("expired").is(false),
-                Criteria.where("revoked").is(false)
-            )
-        );
-
-        return mongoTemplate.find(query, Token.class);
-    }
-
-    @Override
-    public Optional<Token> findByToken(String token) {
-        Query query = new Query(Criteria.where("token").is(token));
-        Token foundToken = mongoTemplate.findOne(query, Token.class);
-        return Optional.ofNullable(foundToken);
-    }
-
-    @Override
-    public void saveAll(List<Token> tokens) {
-        tokens.forEach(mongoTemplate::save);
-    }
-
-    @Override
-    public void save(Token token) {
-        mongoTemplate.save(token);
-    }
+public interface ITokenRepository extends MongoRepository<Token, String> {
+    Optional<Token> findByToken(String token);
+    Optional<Token> findByUser(User user);
 }
