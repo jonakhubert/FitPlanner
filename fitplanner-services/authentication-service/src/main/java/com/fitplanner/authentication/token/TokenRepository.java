669ec63178898f0d0ecfacdc2a2b4aca292da496
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-27 14:19:07 +0200

    Add find by user email method

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/token/TokenRepository.java
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/token/TokenRepository.java
@@ -1,13 +1,12 @@
 package com.fitplanner.authentication.token;
 
-import com.fitplanner.authentication.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ITokenRepository extends MongoRepository<Token, String> {
+public interface TokenRepository extends MongoRepository<Token, String> {
     Optional<Token> findByToken(String token);
-    Optional<Token> findByUser(User user);
+    Optional<Token> findByUserEmail(String userEmail);
 }
