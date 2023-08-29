6c6f0e0e278ff148918fc0526773e6dc293587b4
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-29 17:22:05 +0200

    Add rest template bean

--- fitplanner-services/api-gateway/src/main/java/com/fitplanner/apigateway/config/SecurityConfig.java
+++ fitplanner-services/api-gateway/src/main/java/com/fitplanner/apigateway/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,5 +15,10 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
