be20b959c250eac95571d12c1eaa3a4db1cd01dc
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-08-29 19:16:41 +0200

    Add web client builder bean

--- fitplanner-services/api-gateway/src/main/java/com/fitplanner/apigateway/config/SecurityConfig.java
+++ fitplanner-services/api-gateway/src/main/java/com/fitplanner/apigateway/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,7 +18,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
