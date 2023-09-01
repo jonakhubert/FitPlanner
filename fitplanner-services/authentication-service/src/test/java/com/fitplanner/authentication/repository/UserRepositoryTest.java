d77c3c44cf34fac84746c660eb19dccdbf94f021
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-01 13:50:57 +0200

    Add user repository tests

--- fitplanner-services/authentication-service/src/test/java/com/fitplanner/authentication/repository/UserRepositoryTest.java
+++ fitplanner-services/authentication-service/src/test/java/com/fitplanner/authentication/repository/UserRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.fitplanner.authentication.repository;
 
 import com.fitplanner.authentication.MongoDBContainerConfig;
+import com.fitplanner.authentication.model.user.Role;
+import com.fitplanner.authentication.model.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @DataMongoTest
 @Testcontainers
@@ -13,10 +19,43 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class UserRepositoryTest {
 
     @Autowired
-    TokenRepository userRepository;
+    UserRepository underTest;
+
+    private User user;
+
+    @BeforeEach
+    public void beforeEach() {
+       user = new User(
+            "John", "Smith",
+            "johnsmith@gmail.com",
+            "1234",
+            Role.USER
+        );
+    }
 
     @Test
-    public void findByEmail() {
+    public void shouldReturnExistingUserWhenExistingEmailIsProvided() {
+        // given
+        String email = "johnsmith@gmail.com";
+        underTest.save(user);
 
+        // when
+        User result = underTest.findByEmail(email).orElse(null);
+
+        // then
+        assertEquals(user, result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenNonExistingEmailIsProvided() {
+        // given
+        String email = "non-existing";
+        underTest.save(user);
+
+        // when
+        User result = underTest.findByEmail(email).orElse(null);
+
+        // then
+        assertNull(result);
     }
 }
