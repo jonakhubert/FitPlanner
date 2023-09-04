fba9a688ef0228b82a604891568f9de50be65e45
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-04 18:35:10 +0200

    Add login function

--- fitplanner-app/src/app/component/login/login.component.ts
+++ fitplanner-app/src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  
+  public loginForm!: FormGroup;
 
+  constructor(
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  public login() {
+    console.log(this.loginForm?.value);
+    this.authenticationService.login(this.loginForm.value).subscribe((response) => {
+      console.log(response);
+    })
+  }
 }
