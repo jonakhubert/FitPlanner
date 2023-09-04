0c79be1e8508c87a1a997c449be74a1f09cd430d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-04 18:35:26 +0200

    Add register function

--- fitplanner-app/src/app/component/register/register.component.ts
+++ fitplanner-app/src/app/component/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterRequest } from 'src/app/interface/register-request';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  
+  public registerForm!: FormGroup;
 
+  constructor(
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.confirmationValidator })
+  }
+
+  private confirmationValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    if(password !== confirmPassword)
+      formGroup.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+    else
+      formGroup.get('confirmPassword')?.setErrors(null);
+  }
+
+  private extractRegisterRequest(formGroup: FormGroup): RegisterRequest {
+    return {
+      firstName: this.registerForm.get('firstName')?.value,
+      lastName: this.registerForm.get('lastName')?.value,
+      email: this.registerForm.get('email')?.value,
+      password: this.registerForm.get('password')?.value,
+    };
+  }
+
+  public register() {
+    console.log(this.registerForm?.value);
+    this.authenticationService.register(this.extractRegisterRequest(this.registerForm)).subscribe((response) => {
+      console.log(response);
+    })
+  }
 }
