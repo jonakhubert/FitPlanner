c212b9d4431cd8974a69fe231361e09efdd1887f
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-05 15:40:06 +0200

    Add exception handling

--- fitplanner-app/src/app/service/authentication.service.ts
+++ fitplanner-app/src/app/service/authentication.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { RegisterRequest } from '../interface/register-request';
 import { AuthenticationResponse } from '../interface/authentication-response';
 import { LoginRequest } from '../interface/login-request';
+import { ApiError } from '../interface/api-error';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +16,31 @@ export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
   public register(request: RegisterRequest): Observable<AuthenticationResponse> {
-    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/register`, request);
+    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/register`, request)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          throw this.buildApiError(error);
+        })
+      );
   }
 
   public login(request: LoginRequest): Observable<AuthenticationResponse> {
-    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/authenticate`, request);
+    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/login`, request)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        throw this.buildApiError(error);
+      })
+    );
+  }
+
+  private buildApiError(error: HttpErrorResponse) {
+    const apiError: ApiError = {
+      path: error.error.path,
+      message: error.error.message,
+      statusCode: error.error.statusCode,
+      time: error.error.timestamp
+    };
+
+    return apiError;
   }
 }
