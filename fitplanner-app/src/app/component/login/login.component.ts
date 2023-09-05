0ca89af74aace453ecbc65132143db2c749c8f3b
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-05 15:41:46 +0200

    Add login form validation

--- fitplanner-app/src/app/component/login/login.component.ts
+++ fitplanner-app/src/app/component/login/login.component.ts
@@ -10,6 +10,8 @@ import { AuthenticationService } from 'src/app/service/authentication.service';
 export class LoginComponent {
   
   public loginForm!: FormGroup;
+  submitted = false;
+  message = '';
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -18,15 +20,31 @@ export class LoginComponent {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+      + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")]],
       password: ['', Validators.required]
     })
   }
 
-  public login() {
-    console.log(this.loginForm?.value);
-    this.authenticationService.login(this.loginForm.value).subscribe((response) => {
-      console.log(response);
-    })
+  onSubmit() {
+    this.submitted = true;
+
+    if(this.loginForm.invalid) {
+      return;
+    }
+
+    this.authenticationService.login(this.loginForm.value).subscribe(
+    {
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        if(error.statusCode === 404) {
+          this.message = this.loginForm.get('email')?.value + " doesn't exist.";
+        } else if(error.statusCode === 400) {
+          this.message = "Invalid password.";
+        }
+      }
+    });
   }
 }
