671e80bc127cc08311b634d42b0bbbc04a260229
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-06 17:41:30 +0200

    Add auth module to imports and remove unused declarations

--- fitplanner-app/src/app/app.module.ts
+++ fitplanner-app/src/app/app.module.ts
@@ -3,26 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './component/register/register.component';
-import { LoginComponent } from './component/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MainNavComponent } from './component/main-nav/main-nav.component';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent,
     MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
