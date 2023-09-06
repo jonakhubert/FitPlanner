9518aee2528b57388059e0cf829e549299819628
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-06 17:39:15 +0200

    Add routing for auth module

--- fitplanner-app/src/app/app-routing.module.ts
+++ fitplanner-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './component/register/register.component';
-import { LoginComponent } from './component/login/login.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/component/login/login.component';
+import { RegisterComponent } from './auth/component/register/register.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  {
+    path: 'auth',  component: AuthComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
 ];
 
 @NgModule({
