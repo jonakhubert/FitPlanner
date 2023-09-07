db63d97025e0c998084085720a41c0db5f4321c9
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-07 20:11:42 +0200

    Add routing for user module

--- fitplanner-app/src/app/app-routing.module.ts
+++ fitplanner-app/src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'user',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./modules/user/user.module').then(m => m.UserModule)
+  },
   { path: '**', component: NotFoundComponent }
 ];
 
