400c1ec017d1fc8ec76170af79b5bf92238d40a7
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-07 20:10:48 +0200

    Remove main nav component

--- fitplanner-app/src/app/app.module.ts
+++ fitplanner-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainNavComponent } from './component/main-nav/main-nav.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -16,7 +15,6 @@ import { NotFoundComponent } from './component/not-found/not-found.component';
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    MainNavComponent,
     NotFoundComponent
   ],
   imports: [
