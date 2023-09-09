b02c367307668695ea21b0a09e6899ec43be8341
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-09 19:14:25 +0200

    Add email service

---
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/service/EmailService.java
@@ -0,0 +1,36 @@
+package com.fitplanner.authentication.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Async
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email.");
+            helper.setFrom("support@fitplanner.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new IllegalStateException("Failed to send an email.");
+        }
+    }
+}
