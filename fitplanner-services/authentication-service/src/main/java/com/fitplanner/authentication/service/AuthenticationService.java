30416870809b6aa4c87c2efc21a007a09b8dbae9
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-09 19:19:02 +0200

    Add email verification before login

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/service/AuthenticationService.java
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/service/AuthenticationService.java
@@ -4,51 +4,95 @@ import com.fitplanner.authentication.exception.model.InvalidEmailFormatException
 import com.fitplanner.authentication.exception.model.UserAlreadyExistException;
 import com.fitplanner.authentication.exception.model.UserNotFoundException;
 import com.fitplanner.authentication.model.api.LoginRequest;
-import com.fitplanner.authentication.model.api.AuthenticationResponse;
+import com.fitplanner.authentication.model.api.LoginResponse;
 import com.fitplanner.authentication.model.api.RegisterRequest;
-import com.fitplanner.authentication.repository.TokenRepository;
-import com.fitplanner.authentication.model.token.Token;
+import com.fitplanner.authentication.model.api.ConfirmationResponse;
+import com.fitplanner.authentication.model.confirmationtoken.ConfirmationToken;
+import com.fitplanner.authentication.repository.AccessTokenRepository;
+import com.fitplanner.authentication.model.accesstoken.AccessToken;
 import com.fitplanner.authentication.model.user.Role;
 import com.fitplanner.authentication.model.user.User;
+import com.fitplanner.authentication.repository.ConfirmationTokenRepository;
 import com.fitplanner.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 @Service
 public class AuthenticationService {
 
     private final UserRepository userRepository;
-    private final TokenRepository tokenRepository;
+    private final AccessTokenRepository accessTokenRepository;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+    private final ConfirmationTokenService confirmationTokenService;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final EmailService emailService;
 
     @Autowired
     public AuthenticationService(
         UserRepository userRepository,
-        TokenRepository tokenRepository,
+        AccessTokenRepository accessTokenRepository,
+        ConfirmationTokenRepository confirmationTokenRepository,
+        ConfirmationTokenService confirmationTokenService,
         PasswordEncoder passwordEncoder,
         JwtService jwtService,
-        AuthenticationManager authenticationManager
+        AuthenticationManager authenticationManager,
+        EmailService emailService
     ) {
         this.userRepository = userRepository;
-        this.tokenRepository = tokenRepository;
+        this.accessTokenRepository = accessTokenRepository;
+        this.confirmationTokenRepository = confirmationTokenRepository;
+        this.confirmationTokenService = confirmationTokenService;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+        this.emailService = emailService;
     }
 
-    public AuthenticationResponse register(RegisterRequest registerRequest) {
+    public ConfirmationResponse register(RegisterRequest registerRequest) {
+        String token = UUID.randomUUID().toString();
+        String link = "http://localhost:8222/api/auth/confirm?token=" + token;
+
         if(!isEmailValid(registerRequest.email()))
             throw new InvalidEmailFormatException(registerRequest.email() + " format is invalid.");
 
-        if(userRepository.findByEmail(registerRequest.email()).isPresent())
+        Optional<User> newUser = userRepository.findByEmail(registerRequest.email());
+
+        if(newUser.isPresent() && newUser.get().isEnabled())
             throw new UserAlreadyExistException(registerRequest.email() + " already exist.");
+
+        if(newUser.isPresent() && !newUser.get().isEnabled()) {
+            emailService.send(
+                    registerRequest.email(),
+                    buildEmail(registerRequest.firstName(), link)
+            );
+
+            confirmationTokenRepository.findByUserEmail(registerRequest.email()).ifPresent(confirmationTokenRepository::delete);
+            ConfirmationToken confirmationToken = new ConfirmationToken(
+                    token,
+                    LocalDateTime.now(),
+                    LocalDateTime.now().plusMinutes(15),
+                    registerRequest.email()
+            );
+            confirmationTokenService.saveToken(confirmationToken);
+
+            return new ConfirmationResponse("Confirmation email has been resend.");
+        }
+
+        emailService.send(
+            registerRequest.email(),
+            buildEmail(registerRequest.firstName(), link)
+        );
 
         User user = new User(
             registerRequest.firstName(),
@@ -59,19 +103,27 @@ public class AuthenticationService {
         );
 
         User savedUser = userRepository.save(user);
-        String jwt = jwtService.generateToken(user);
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+            token,
+            LocalDateTime.now(),
+            LocalDateTime.now().plusMinutes(15),
+            savedUser.getUsername()
+        );
 
-        saveUserToken(jwt, savedUser);
+        confirmationTokenService.saveToken(confirmationToken);
 
-        return new AuthenticationResponse(jwt);
+        return new ConfirmationResponse(token);
     }
 
-    public AuthenticationResponse login(LoginRequest loginRequest) {
+    public LoginResponse login(LoginRequest loginRequest) {
         if(!isEmailValid(loginRequest.email()))
             throw new InvalidEmailFormatException(loginRequest.email() + " format is invalid.");
 
         User user = userRepository.findByEmail(loginRequest.email())
-                .orElseThrow(() -> new UserNotFoundException("User not found."));
+            .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        if(!user.isEnabled())
+            throw new IllegalStateException("User is not enabled.");
 
         authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
@@ -85,20 +137,38 @@ public class AuthenticationService {
         deleteUserToken(user);
         saveUserToken(jwt, user);
 
-        return new AuthenticationResponse(jwt);
+        return new LoginResponse(jwt);
+    }
+
+    @Transactional
+    public ConfirmationResponse confirmToken(String token) {
+        ConfirmationToken confirmationToken = confirmationTokenService.getToken(token);
+
+        if(confirmationToken.getConfirmedAt() != null)
+            throw new IllegalStateException("email already confirmed");
+
+        LocalDateTime expiredAt = confirmationToken.getExpiredAt();
+
+        if (expiredAt.isBefore(LocalDateTime.now()))
+            throw new IllegalStateException("token expired");
+
+        confirmationTokenService.setConfirmedAt(token);
+        verifyUser(confirmationToken.getUserEmail());
+
+        return new ConfirmationResponse("User account verified.");
     }
 
     public boolean isTokenValid(String token) {
-        return tokenRepository.findByToken(token).isPresent();
+        return accessTokenRepository.findByToken(token).isPresent();
     }
 
     private void saveUserToken(String jwt, User user) {
-        Token token = new Token(jwt, user.getUsername());
-        tokenRepository.save(token);
+        AccessToken token = new AccessToken(jwt, user.getUsername());
+        accessTokenRepository.save(token);
     }
 
     private void deleteUserToken(User user) {
-        tokenRepository.findByUserEmail(user.getUsername()).ifPresent(tokenRepository::delete);
+        accessTokenRepository.findByUserEmail(user.getUsername()).ifPresent(accessTokenRepository::delete);
     }
 
     private boolean isEmailValid(String email) {
@@ -108,5 +178,82 @@ public class AuthenticationService {
         return Pattern.compile(regexPattern)
             .matcher(email)
             .matches();
+    }
+
+    private void verifyUser(String email) {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        user.setEnabled(true);
+        userRepository.save(user);
+    }
+
+    private String buildEmail(String name, String link) {
+        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                "      <td>\n" +
+                "        \n" +
+                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 15 minutes. <p>See you soon</p>" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
     }
 }
