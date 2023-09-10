0e03640ce3368033b77c61cb76bf0ce7f5b3b545
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-10 17:54:36 +0200

    Add min password length

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/model/api/RegisterRequest.java
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/model/api/RegisterRequest.java
@@ -2,10 +2,11 @@ package com.fitplanner.authentication.model.api;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotNull @NotEmpty String firstName,
         @NotNull @NotEmpty String lastName,
         @NotNull @NotEmpty String email,
-        @NotNull @NotEmpty String password
+        @NotNull @NotEmpty @Size(min = 6) String password
 ) {}
