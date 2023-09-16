f0d376d00383b1f6e362113e7e819baf8789ba30
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-17 00:32:08 +0200

    Delete unused components

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/model/api/RegisterResponse.java
+++
@@ -1,7 +0,0 @@
-package com.fitplanner.authentication.model.api;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record RegisterResponse(
-        @JsonProperty("verification_message") String message
-) {}
