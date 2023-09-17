5d9687952d6a60eaca64df0c81c83cabb4c8f7c4
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-17 16:35:08 +0200

    Authorize user after refresh page and router link change

--- fitplanner-app/src/app/modules/user/component/user-dashboard/user-dashboard.component.ts
+++ fitplanner-app/src/app/modules/user/component/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class UserDashboardComponent {
 
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.authorize();
+      }
+    });
+
+    this.authorize();
+  }
+
+  private authorize() {
+    this.authenticationService.authorize().subscribe({
+      next: () => {},
+      error: () => {
+        this.toastr.info("Your session has expired.", "Info");
+      }
+    });
+  }
 }
