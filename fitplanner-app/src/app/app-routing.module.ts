21a7dd7a1a6968269783ae4c6d18763a8216372d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-17 15:45:09 +0200

    Add reset password component to app routing

--- fitplanner-app/src/app/app-routing.module.ts
+++ fitplanner-app/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 import { authGuard } from './guards/auth.guard';
+import { ResetPasswordComponent } from './component/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'user',
