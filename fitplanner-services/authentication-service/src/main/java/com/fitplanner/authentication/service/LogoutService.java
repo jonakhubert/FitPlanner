5196cd48ce8878a31ed4e8f5f37bacbd7cc8282f
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-24 00:11:12 +0200

    Handle user not found exception

--- fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/service/LogoutService.java
+++ fitplanner-services/authentication-service/src/main/java/com/fitplanner/authentication/service/LogoutService.java
@@ -1,31 +1,61 @@
 package com.fitplanner.authentication.service;
 
-import com.fitplanner.authentication.repository.AccessTokenRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fitplanner.authentication.exception.model.UserNotFoundException;
+import com.fitplanner.authentication.model.api.ApiError;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 @Service
 public class LogoutService implements LogoutHandler {
 
-    private final AccessTokenRepository accessTokenRepository;
+    private final UserService userService;
 
     @Autowired
-    public LogoutService(AccessTokenRepository accessTokenRepository) {
-        this.accessTokenRepository = accessTokenRepository;
+    public LogoutService(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        final var authHeader = request.getHeader("Authorization");
+        try {
+            final var authHeader = request.getHeader("Authorization");
 
-        if(authHeader == null || !authHeader.startsWith("Bearer "))
-           return;
+            if(authHeader == null || !authHeader.startsWith("Bearer "))
+                return;
 
-        var token = authHeader.substring(7);
-        accessTokenRepository.findByToken(token).ifPresent(accessTokenRepository::delete);
+            var token = authHeader.substring(7);
+            userService.revokeAccessToken(token);
+        } catch(RuntimeException ex){
+            handleUserNotFoundException(ex, request, response);
+        }
+    }
+
+    private void handleUserNotFoundException(Exception ex, HttpServletRequest request, HttpServletResponse response) {
+        var apiError = new ApiError(
+            request.getRequestURI(),
+            ex.getMessage(),
+            HttpStatus.UNAUTHORIZED.value(),
+            LocalDateTime.now().toString()
+        );
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        var objectMapper = new ObjectMapper();
+        try {
+            response.getWriter().write(objectMapper.writeValueAsString(apiError));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
