31fd6e85db72bf3c9a3e8c8a0a13803ebf5a9201
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-24 18:17:34 +0200

    Add delete account method

--- fitplanner-app/src/app/modules/user/component/account-delete/account-delete.component.ts
+++ fitplanner-app/src/app/modules/user/component/account-delete/account-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-account-delete',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AccountDeleteComponent {
 
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  delete() {
+    const email = localStorage.getItem('userEmail');
+    const closeButton = document.getElementById("closeBtn");
+
+    this.userService.deleteAccount(email!).subscribe(
+    {
+      next: () => {
+        closeButton?.click();
+        this.router.navigate(['login']);
+        this.toastr.info("Your account has been permanently deleted.", "Info");
+      },
+      error: () => {
+        this.toastr.error("Something went wrong. Try again later.", "Error");
+      }
+    });
+    }
 }
