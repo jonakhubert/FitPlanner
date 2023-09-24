1f21b39c9c323b473c092d026a7d3d6417f13375
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-24 17:22:27 +0200

    Check token validation

--- fitplanner-app/src/app/guards/auth/auth.guard.ts
+++ fitplanner-app/src/app/guards/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from '../../service/authentication.service';
+import { Observable } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
@@ -9,6 +10,23 @@ export const authGuard: CanActivateFn = (route, state) => {
   if(!authService.isLoggedIn()) {
     router.navigate(['login']);
     return false;
+  }
+
+  if(authService.isLoggedIn()) {
+    return new Observable<boolean>((observer) => {
+      authService.authorize().subscribe(
+      {
+        next: () => {
+          observer.next(true);
+          observer.complete();
+        },
+        error: () => {
+          router.navigate(['/login']);
+          observer.next(false);
+          observer.complete();
+        }
+      });
+    });
   }
 
   return authService.isLoggedIn();
