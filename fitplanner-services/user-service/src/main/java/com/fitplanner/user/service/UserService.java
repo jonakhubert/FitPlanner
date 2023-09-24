a304b7f32481a4071b7846765641c23ff5031e4c
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-24 17:21:07 +0200

    Add delete account method

--- fitplanner-services/user-service/src/main/java/com/fitplanner/user/service/UserService.java
+++ fitplanner-services/user-service/src/main/java/com/fitplanner/user/service/UserService.java
@@ -7,6 +7,7 @@ import com.fitplanner.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -20,6 +21,7 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Transactional
     public ConfirmationResponse changePassword(ChangePasswordRequest request) {
         var user = userRepository.findByEmail(request.email())
             .orElseThrow(() -> new UserNotFoundException("User not found."));
@@ -28,5 +30,15 @@ public class UserService {
         userRepository.save(user);
 
         return new ConfirmationResponse("Password has been changed.");
+    }
+
+    @Transactional
+    public ConfirmationResponse deleteAccount(String email) {
+        var user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        userRepository.delete(user);
+
+        return new ConfirmationResponse("Account has been deleted.");
     }
 }
