256949a9d6c64cbbe38f789f6b9a65171c67854d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-25 14:09:07 +0200

    Remove redundant logged in check

--- fitplanner-app/src/app/guards/auth/auth.guard.ts
+++ fitplanner-app/src/app/guards/auth/auth.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from '../../service/authentication.service';
-import { Observable } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
@@ -10,23 +9,6 @@ export const authGuard: CanActivateFn = (route, state) => {
   if(!authService.isLoggedIn()) {
     router.navigate(['login']);
     return false;
-  }
-
-  if(authService.isLoggedIn()) {
-    return new Observable<boolean>((observer) => {
-      authService.authorize().subscribe(
-      {
-        next: () => {
-          observer.next(true);
-          observer.complete();
-        },
-        error: () => {
-          router.navigate(['/login']);
-          observer.next(false);
-          observer.complete();
-        }
-      });
-    });
   }
 
   return authService.isLoggedIn();
