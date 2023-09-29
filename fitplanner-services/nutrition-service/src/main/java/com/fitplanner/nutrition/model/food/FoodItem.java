061d8ae0e84faf84cf26e72096a129b8fd580e32
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-29 21:54:51 +0200

    Change from record to class

--- fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/model/food/FoodItem.java
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/model/food/FoodItem.java
@@ -1,10 +1,26 @@
 package com.fitplanner.nutrition.model.food;
 
-public record FoodItem(
-        String name,
-        double calories,
-        double protein,
-        double fat,
-        double carbs,
-        double quantity
-) {}
+public class FoodItem {
+
+    private String name;
+    private double calories;
+    private double protein;
+    private double fat;
+    private double carbs;
+    private double quantity;
+
+    // getters
+    public String getName() { return name; }
+    public double getCalories() { return calories; }
+    public double getProtein() { return protein; }
+    public double getFat() { return fat; }
+    public double getCarbs() { return carbs; }
+    public double getQuantity() { return quantity; }
+
+    // setters
+    public void setCalories(double calories) { this.calories = calories; }
+    public void setProtein(double protein) { this.protein = protein; }
+    public void setFat(double fat) { this.fat = fat; }
+    public void setCarbs(double carbs) { this.carbs = carbs; }
+    public void setQuantity(double quantity) { this.quantity = quantity; }
+}
