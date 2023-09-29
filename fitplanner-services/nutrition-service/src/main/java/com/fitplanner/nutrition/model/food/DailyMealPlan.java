4d3d48b636a27dc4b23021f8613b4fce47c98568
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-29 18:43:17 +0200

    Add daily meal plan model

--- fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/model/food/DailyMealPlan.java
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/model/food/DailyMealPlan.java
@@ -1,11 +1,11 @@
-package com.fitplanner.nutrition.model;
+package com.fitplanner.nutrition.model.food;
 
 import com.fitplanner.nutrition.model.user.User;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Document(value = "daily_meal_plans")
@@ -13,12 +13,19 @@ public class DailyMealPlan {
 
     @Id
     private String id;
-    private LocalDateTime date;
+    private String date;
     private List<Meal> meals;
 
     @DocumentReference(lazy = true)
     private User user;
 
+    public DailyMealPlan(User user, String date) {
+        this.date = date;
+        this.meals = new ArrayList<>();
+        this.user = user;
+    }
+
     // getters
     public List<Meal> getMeals() { return meals; }
+    public String getDate() { return date; }
 }
