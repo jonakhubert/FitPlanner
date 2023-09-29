414e2293024df8319154ed6bdf9adfbb7634dc1d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-29 18:45:57 +0200

    Add user repository

---
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.fitplanner.nutrition.repository;
+
+import com.fitplanner.nutrition.model.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
