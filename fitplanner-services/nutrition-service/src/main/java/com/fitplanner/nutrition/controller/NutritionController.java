2e71bc087bf1d4c500b731a6b96c64d32df15b40
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-29 18:42:58 +0200

    Create add meal method

--- fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/controller/NutritionController.java
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/controller/NutritionController.java
@@ -1,9 +1,11 @@
 package com.fitplanner.nutrition.controller;
 
+import com.fitplanner.nutrition.model.api.MealRequest;
+import com.fitplanner.nutrition.service.NutritionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,10 +14,26 @@ import java.util.Map;
 @RequestMapping(path = "/api/nutrition")
 public class NutritionController {
 
+    private final NutritionService nutritionService;
+
+    @Autowired
+    public NutritionController(NutritionService nutritionService) {
+        this.nutritionService = nutritionService;
+    }
+
     @GetMapping(path = "/hello")
     public ResponseEntity<Map<String, String>> hello() {
         Map<String, String> response = new HashMap<>();
         response.put("message", "Hello from secure nutrition endpoint.");
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(
+        path = "/add-meal",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> addMeal(@RequestBody MealRequest request) {
+        nutritionService.addMeal(request);
+        return ResponseEntity.ok("super");
     }
 }
