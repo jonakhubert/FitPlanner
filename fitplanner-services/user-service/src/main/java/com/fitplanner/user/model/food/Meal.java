82494144f3aa90d9681076ceb192140769e45c78
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-30 19:32:05 +0200

    Change class to record

--- fitplanner-services/user-service/src/main/java/com/fitplanner/user/model/food/Meal.java
+++ fitplanner-services/user-service/src/main/java/com/fitplanner/user/model/food/Meal.java
@@ -1,18 +1,8 @@
 package com.fitplanner.user.model.food;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Meal {
-    private String name;
-    private List<FoodItem> foodItems;
-
-    public Meal(String name) {
-        this.name = name;
-        this.foodItems = new ArrayList<>();
-    }
-
-    // getters
-    public String getName() { return name; }
-    public List<FoodItem> getFoodItems() { return foodItems; }
-}
+public record Meal(
+        String name,
+        List<FoodItem> foodItems
+) {}
