38acb8347d5884bc58e302db247e424a37c7ba87
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-09-30 19:35:36 +0200

    Add required getters and setters

--- fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/model/user/User.java
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/model/user/User.java
@@ -1,43 +1,23 @@
 package com.fitplanner.nutrition.model.user;
 
 import com.fitplanner.nutrition.model.food.DailyMealPlan;
-import com.fitplanner.nutrition.model.tokens.ResetPasswordToken;
-import com.fitplanner.nutrition.model.tokens.VerificationToken;
-import com.fitplanner.nutrition.model.tokens.accesstoken.AccessToken;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(value = "users")
 public class User {
 
-    @Id
-    private String id;
-    private String firstName;
-    private String lastName;
     private String email;
-    private String password;
-    @Enumerated(EnumType.STRING)
-    private Role role;
-    private Boolean enabled;
-
-    // tokens
-    private AccessToken accessToken;
-    private VerificationToken verificationToken;
-    private ResetPasswordToken resetPasswordToken;
-
     // preferences TODO: add other parameters (height, weight, etc.)
     private double calories;
     private List<DailyMealPlan> dailyMealPlans;
 
     // getters
-    public List<DailyMealPlan> getDailyMealPlans() { return dailyMealPlans; }
     public String getEmail() { return email; }
+    public List<DailyMealPlan> getDailyMealPlans() { return dailyMealPlans; }
+    public double getCalories() { return calories; }
 
     // setters
+    public void setEmail(String email) { this.email = email; }
     public void setCalories(double calories) { this.calories = calories; }
     public void setDailyMealPlans(List<DailyMealPlan> dailyMealPlans) { this.dailyMealPlans = dailyMealPlans; }
 }
