a6ec92e587e097dc005def1499902bbd760acb60
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-04 21:19:47 +0200

    Add weight, height and goal fields

--- fitplanner-app/src/app/interface/register-request.ts
+++ fitplanner-app/src/app/interface/register-request.ts
@@ -1,6 +1,9 @@
 export interface RegisterRequest {
     firstName: string,
     lastName: string,
+    height: number,
+    weight: number,
+    goal: number,
     email: string,
     password: string
 }
