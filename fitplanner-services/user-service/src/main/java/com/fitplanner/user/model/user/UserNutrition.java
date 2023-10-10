b8018dae50bf24098648b28aa2e413230978c9ed
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-10 20:35:34 +0200

    Remove redundant comment

--- fitplanner-services/user-service/src/main/java/com/fitplanner/user/model/user/UserNutrition.java
+++ fitplanner-services/user-service/src/main/java/com/fitplanner/user/model/user/UserNutrition.java
@@ -4,7 +4,6 @@ import com.fitplanner.user.model.food.DailyMealPlan;
 
 import java.util.List;
 
-// preferences TODO: add other parameters (height, weight, etc.)
 public record UserNutrition(
         String email,
         double calories,
