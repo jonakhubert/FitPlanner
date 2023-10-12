2ec6026910bb06bb8ad8d7c6dcef915d7fbeb021
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-12 19:33:20 +0200

    Update paths

--- fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/client/UserServiceClient.java
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/client/UserServiceClient.java
@@ -4,12 +4,12 @@ import com.fitplanner.nutrition.model.user.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "user-service", url = "http://localhost:8222/api/user")
+@FeignClient(value = "user-service", url = "http://localhost:8222/api/users")
 public interface UserServiceClient {
 
-    @GetMapping(path = "/get")
-    User getUser(@RequestParam("email") String email, @RequestHeader("Authorization") String header);
+    @GetMapping(path = "/{email}")
+    User getUser(@PathVariable("email") String email, @RequestHeader("Authorization") String header);
 
-    @PostMapping(path = "/save-user-nutrition")
+    @PostMapping(path = "/nutrition")
     void saveUserNutrition(@RequestBody User user, @RequestHeader("Authorization") String header);
 }
