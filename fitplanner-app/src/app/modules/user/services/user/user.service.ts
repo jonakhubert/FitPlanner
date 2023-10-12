2e14e64b78ec9a4e98892fc9be07abc635693321
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-12 19:43:59 +0200

    Add get user method

--- fitplanner-app/src/app/modules/user/services/user/user.service.ts
+++ fitplanner-app/src/app/modules/user/services/user/user.service.ts
@@ -3,15 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable, catchError, tap } from 'rxjs';
 import { ChangePasswordRequest } from 'src/app/modules/user/interface/change-password-request';
 import { ConfirmationResponse } from 'src/app/interface/confirmation-response';
+import { User } from '../../interface/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private readonly apiUrl = 'http://localhost:8222/api/user';
+  private readonly apiUrl = 'http://localhost:8222/api/users';
 
   constructor(private http: HttpClient) {}
+
+  public getUser(email: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/get?email=${email}`)
+  }
 
   public changePassword(request: ChangePasswordRequest): Observable<ConfirmationResponse> {
     return this.http.post<ConfirmationResponse>(`${this.apiUrl}/change-password`, request)
