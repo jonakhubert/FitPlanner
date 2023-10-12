e1863203725d0202a0ff814a5c9800fb97e636cf
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-12 19:36:00 +0200

    Add user dto model

---
+++ fitplanner-services/user-service/src/main/java/com/fitplanner/user/model/user/UserDTO.java
@@ -0,0 +1,13 @@
+package com.fitplanner.user.model.user;
+
+import com.fitplanner.user.model.food.DailyMealPlan;
+
+import java.util.List;
+
+public record UserDTO(
+        String firstName,
+        String lastName,
+        String email,
+        NutritionInfo nutritionInfo,
+        List<DailyMealPlan> dailyMealPlans
+) {}
