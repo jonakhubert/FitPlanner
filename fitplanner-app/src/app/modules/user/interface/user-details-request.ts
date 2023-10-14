08a0c5234b42e0f8d45595b3c9eaab9a2d2d1fcc
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-14 20:25:03 +0200

    Add user details request model

---
+++ fitplanner-app/src/app/modules/user/interface/user-details-request.ts
@@ -0,0 +1,9 @@
+export interface UserDetailsRequest {
+    firstName: string,
+    lastName: string,
+    email: string,
+    height: number,
+    weight: number,
+    goal: number,
+    activity_level: number
+}
