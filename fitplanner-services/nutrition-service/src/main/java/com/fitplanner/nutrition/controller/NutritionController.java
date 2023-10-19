ecfce93083825294fad7d02a08f27392d6e7cc00
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-19 19:17:11 +0200

    Add endpoint for getting products by name

--- fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/controller/NutritionController.java
+++ fitplanner-services/nutrition-service/src/main/java/com/fitplanner/nutrition/controller/NutritionController.java
@@ -4,12 +4,15 @@ import com.fitplanner.nutrition.model.api.ConfirmationResponse;
 import com.fitplanner.nutrition.model.api.FoodItemCreationRequest;
 import com.fitplanner.nutrition.model.api.FoodItemRemovalRequest;
 import com.fitplanner.nutrition.model.food.DailyMealPlan;
+import com.fitplanner.nutrition.model.food.Product;
 import com.fitplanner.nutrition.service.NutritionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/nutrition")
@@ -55,4 +58,20 @@ public class NutritionController {
     ) {
         return ResponseEntity.ok(nutritionService.getDailyMealPlan(email, date, header));
     }
+
+    @GetMapping(
+        path = "/products",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<Product>> getProducts(@RequestParam("name") String name) {
+        return ResponseEntity.ok(nutritionService.getProducts(name));
+    }
+
+//    @PostMapping(
+//        path = "/product"
+//    )
+//    public void addProduct(@RequestBody ProductRequest request) {
+//        var product = new Product(request.name(), request.calories(), request.protein(), request.fat(), request.carbs());
+//        productRepository.save(product);
+//    }
 }
