3a754a8ae2dc6f603f1135c487d5809d53a9431f
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-26 21:35:09 +0200

    Add required getters and setters

--- fitplanner-services/workout-service/src/main/java/com/fitplanner/workout/model/training/WorkoutPlan.java
+++ fitplanner-services/workout-service/src/main/java/com/fitplanner/workout/model/training/WorkoutPlan.java
@@ -1,5 +1,6 @@
 package com.fitplanner.workout.model.training;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WorkoutPlan {
@@ -7,12 +8,23 @@ public class WorkoutPlan {
     private String date;
     private List<ExerciseInfo> exerciseInfos;
 
+    public WorkoutPlan(String date) {
+        this.date = date;
+        this.exerciseInfos = new ArrayList<>();
+    }
+
     public WorkoutPlan(String date, List<ExerciseInfo> exerciseInfos) {
         this.date = date;
         this.exerciseInfos = exerciseInfos;
     }
 
+    public WorkoutPlan() {}
+
     // getters
     public String getDate() { return date; }
-    public List<ExerciseInfo> getExerciseList() { return exerciseInfos; }
+    public List<ExerciseInfo> getExerciseInfos() { return exerciseInfos; }
+
+    // setters
+    public void setDate(String date) { this.date = date; }
+    public void setExerciseInfos(List<ExerciseInfo> exerciseInfos) { this.exerciseInfos = exerciseInfos; }
 }
