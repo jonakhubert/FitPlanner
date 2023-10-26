05b3a355e1a2126b8b8379857f51b906f05cc5a9
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-26 21:44:12 +0200

    Add methods for removing exercises and fetching workout plans

--- fitplanner-app/src/app/modules/user/component/workout/workout.component.ts
+++ fitplanner-app/src/app/modules/user/component/workout/workout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { WorkoutService } from '../../services/workout/workout.service';
+import { FormBuilder } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { WorkoutPlan } from '../../interface/workout-plan';
 
 @Component({
   selector: 'app-workout',
@@ -7,9 +11,53 @@ import { Component } from '@angular/core';
 })
 export class WorkoutComponent {
   formattedDate: string = '';
+  workoutPlan: WorkoutPlan | undefined;
+
+  constructor(
+    private workoutService: WorkoutService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchWorkoutPlan();
+  }
 
   onDateSelected(date: string): void {
     this.formattedDate = date;
-    console.log(date);
+    this.fetchWorkoutPlan();
+  }
+
+  removeExerciseInfo(id: string): void {
+    const email = localStorage.getItem('userEmail');
+
+    if(email) {
+      this.workoutService.removeExerciseInfo(email, this.formattedDate, id).subscribe(
+      {
+        next: (response) => {
+          this.ngOnInit();
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+    }
+  }
+
+  private fetchWorkoutPlan(): void {
+    const email = localStorage.getItem("userEmail");
+
+    if(email && this.formattedDate) {
+      this.workoutService.getWorkoutPlan(email, this.formattedDate).subscribe(
+      {
+        next: (response) => {
+          console.log(response);
+          this.workoutPlan = response;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+    }
   }
 }
