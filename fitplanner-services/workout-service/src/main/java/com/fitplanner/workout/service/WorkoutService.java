ef6d309cb7372b337b104c6f6788ae2544d07cf5
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-10-26 21:36:34 +0200

    Add method for retrieving workout plans and adding and removing user exercises

--- fitplanner-services/workout-service/src/main/java/com/fitplanner/workout/service/WorkoutService.java
+++ fitplanner-services/workout-service/src/main/java/com/fitplanner/workout/service/WorkoutService.java
@@ -1,7 +1,11 @@
 package com.fitplanner.workout.service;
 
+import com.fitplanner.workout.client.UserServiceClient;
+import com.fitplanner.workout.model.api.ConfirmationResponse;
 import com.fitplanner.workout.model.api.ExerciseRequest;
 import com.fitplanner.workout.model.training.Exercise;
+import com.fitplanner.workout.model.training.ExerciseInfo;
+import com.fitplanner.workout.model.training.WorkoutPlan;
 import com.fitplanner.workout.repository.ExerciseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,15 +17,62 @@ import java.util.List;
 public class WorkoutService {
 
     private final ExerciseRepository exerciseRepository;
+    private final UserServiceClient userServiceClient;
 
     @Autowired
-    public WorkoutService(ExerciseRepository exerciseRepository) {
+    public WorkoutService(ExerciseRepository exerciseRepository, UserServiceClient userServiceClient) {
         this.exerciseRepository = exerciseRepository;
+        this.userServiceClient = userServiceClient;
     }
 
-    public void addExercise(ExerciseRequest exerciseRequest) {
-        var exercise = new Exercise(exerciseRequest.name(), exerciseRequest.link());
-        exerciseRepository.save(exercise);
+    public ConfirmationResponse addExerciseInfo(String email, String date, ExerciseInfo exerciseInfo, String header) {
+        var user = userServiceClient.getUser(email, header);
+
+        var workoutPlan = user.getWorkoutPlans().stream()
+            .filter(plan -> plan.getDate().equals(date))
+            .findFirst()
+            .orElseGet(() -> {
+               var newPlan = new WorkoutPlan(date);
+               user.getWorkoutPlans().add(newPlan);
+               return newPlan;
+            });
+
+        workoutPlan.getExerciseInfos().add(exerciseInfo);
+        userServiceClient.saveWorkoutPlans(email, user.getWorkoutPlans(), header);
+
+        return new ConfirmationResponse("Exercise has been added.");
+    }
+
+    public ConfirmationResponse removeExerciseInfo(String email, String date, String exerciseInfoId, String header) {
+        var user = userServiceClient.getUser(email, header);
+
+        var userWorkoutPlan = user.getWorkoutPlans().stream()
+            .filter(plan -> plan.getDate().equals(date))
+            .findFirst();
+
+        userWorkoutPlan.ifPresent(workoutPlan -> {
+            workoutPlan.getExerciseInfos().removeIf(exerciseInfo -> exerciseInfo.getId().equals(exerciseInfoId));
+
+            var hasExerciseInfos = user.getWorkoutPlans().stream()
+                .filter(plan -> plan.getDate().equals(date))
+                .anyMatch(plan -> !plan.getExerciseInfos().isEmpty());
+
+            if(!hasExerciseInfos)
+                user.getWorkoutPlans().removeIf(plan -> plan.getDate().equals(date));
+        });
+
+        userServiceClient.saveWorkoutPlans(email, user.getWorkoutPlans(), header);
+
+        return new ConfirmationResponse("Exercise has been removed.");
+    }
+
+    public WorkoutPlan getWorkoutPlan(String email, String date, String header) {
+        var user = userServiceClient.getUser(email, header);
+
+        return user.getWorkoutPlans().stream()
+            .filter(plan -> plan.getDate().equals(date))
+            .findFirst()
+            .orElse(new WorkoutPlan(date));
     }
 
     public List<Exercise> getExercises(String name) {
@@ -30,5 +81,10 @@ public class WorkoutService {
 
         return exerciseRepository.findByNameIgnoreCase(name)
             .orElse(Collections.emptyList());
+    }
+
+    public void addExercise(ExerciseRequest exerciseRequest) {
+        var exercise = new Exercise(exerciseRequest.name(), exerciseRequest.link());
+        exerciseRepository.save(exercise);
     }
 }
